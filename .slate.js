961d0d57745c9bdadcef0e6b095954d8b113a37e
James Blanksby <james@thisismyengine.com>
2021-03-15 10:09:52 +0000

    Updated to latest slate syntax

--- .slate.js
+++ .slate.js
@@ -1,85 +1,132 @@
-/* Define window positions */
+// define attributes
 
-const full = S.op('move', {
+const attribute = {};
+
+// screen size
+attribute.screen = slate.screen().rect();
+
+
+// define layouts
+const layout = {};
+
+// video
+layout.video = slate.layout('video', {
+    'Google Chrome': {
+        operations: [slate.operation('move', {
+            x: 'screenOriginX',
+            y: 'screenOriginY',
+            width: 'floor(screenSizeX * 0.7)',
+            height: 'screenSizeY',
+        }),],
+    },
+    'Messages': {
+        operations: [slate.operation('move', {
+            x: '(screenOriginX + (screenSizeX * 0.7))',
+            y: 'screenOriginY',
+            width: '(screenSizeX * 0.3)',
+            height: 'screenSizeY',
+        }),],
+    },
+});
+
+
+// define operations
+const operation = {};
+
+// video
+operation.video = slate.operation('layout', {
+    name: layout.video,
+});
+
+// fullscreen
+operation.fullscreen = slate.operation('move', {
     x: 'screenOriginX',
     y: 'screenOriginY',
     width: 'screenSizeX',
-    height: 'screenSizeY'
+    height: 'screenSizeY',
 });
 
-const left = S.op('move', {
+// left
+operation.left = slate.operation('move', {
     x: 'screenOriginX',
     y: 'screenOriginY',
-    width: '(screenSizeX/2)',
-    height: 'screenSizeY'
+    width: '(screenSizeX / 2)',
+    height: 'screenSizeY',
 });
 
-const right = S.op('move', {
-    x: '(screenOriginX+(screenSizeX/2))',
+// right
+operation.right = slate.operation('move', {
+    x: '(screenOriginX + (screenSizeX / 2))',
     y: 'screenOriginY',
-    width: '(screenSizeX/2)',
-    height: 'screenSizeY'
+    width: '(screenSizeX / 2)',
+    height: 'screenSizeY',
 });
 
-const middle = () => {
-    if (S.screen().rect().width == 1680) middle_80.run();
-    else middle_50.run();
-};
+// middle
+operation.middle = slate.operation('move', {
+    x: () => {
+        const width = attribute.screen.width;
 
-const middle_50 = S.op('move', {
-    x: '(screenOriginX+(screenSizeX*0.25))',
+        // mbp
+        if (width === 1680) return '(screenOriginX + (screenSizeX * 0.1))';
+        // imac
+        else return '(screenOriginX + (screenSizeX * 0.25))';
+    },
     y: 'screenOriginY',
-    width: '(screenSizeX/2)',
-    height: 'screenSizeY'
+    width: () => {
+        const width = attribute.screen.width;
+
+        // mbp
+        if (width === 1680) return '(screenSizeX * 0.8)';
+        // imac
+        else return '(screenSizeX / 2)';
+    },
+    height: 'screenSizeY',
 });
 
-const middle_80 = S.op('move', {
-    x: '(screenOriginX+(screenSizeX*0.1))',
-    y: 'screenOriginY',
-    width: '(screenSizeX*0.8)',
-    height: 'screenSizeY'
-});
+// quad
+operation.quad = [];
+for (var i = 0; i < 4; i++) {
+    const offset = (0.25 * i);
 
-const quad_0 = S.op('move', {
-    x: 'screenOriginX',
-    y: 'screenOriginY',
-    width: '(screenSizeX/4)',
-    height: 'screenSizeY'
-});
+    const quad = slate.operation('move', {
+        x: `(screenOriginX + (screenSizeX * ${offset}))`,
+        y: 'screenOriginY',
+        width: '(screenSizeX / 4)',
+        height: 'screenSizeY',
+    });
 
-const quad_1 = S.op('move', {
-    x: '(screenOriginX+(screenSizeX*0.25))',
-    y: 'screenOriginY',
-    width: '(screenSizeX/4)',
-    height: 'screenSizeY'
-});
-
-const quad_2 = S.op('move', {
-    x: '(screenOriginX+(screenSizeX*0.5))',
-    y: 'screenOriginY',
-    width: '(screenSizeX/4)',
-    height: 'screenSizeY'
-});
-
-const quad_3 = S.op('move', {
-    x: '(screenOriginX+(screenSizeX*0.75))',
-    y: 'screenOriginY',
-    width: '(screenSizeX/4)',
-    height: 'screenSizeY'
-});
+    operation.quad.push(quad);
+}
 
 
-/* Bind keyboard shortcuts */
+// define keystrokes
 
-S.bnda({
-    'f:cmd;ctrl': full,
-    'left:cmd;ctrl': left,
-    'right:cmd;ctrl': right,
-    'm:cmd;ctrl': middle,
-    '1:cmd;ctrl': quad_0,
-    '2:cmd;ctrl': quad_1,
-    '3:cmd;ctrl': quad_2,
-    '4:cmd;ctrl': quad_3,
-    'left:cmd;alt': quad_0,
-    'right:cmd;alt': quad_3
-});
+// video = cmd + ctrl + v
+slate.bind('v:cmd;ctrl', operation.video);
+
+// fullscreen = cmd + ctrl + f
+slate.bind('f:cmd;ctrl', operation.fullscreen);
+
+// left = cmd + ctrl + ←
+slate.bind('left:cmd;ctrl', operation.left);
+
+// right = cmd + ctrl + →
+slate.bind('right:cmd;ctrl', operation.right);
+
+// middle = cmd + ctrl + m
+slate.bind('m:cmd;ctrl', operation.middle);
+
+// quad 1 = cmd + ctrl + 1 / cmd + alt + ←
+slate.bind('1:cmd;ctrl', operation.quad[0]);
+slate.bind('left:cmd;alt', operation.quad[0]);
+
+// quad 2 = cmd + ctrl + 2
+slate.bind('2:cmd;ctrl', operation.quad[1]);
+
+// quad 3 = cmd + ctrl + 3
+slate.bind('3:cmd;ctrl', operation.quad[2]);
+
+// quad 4 = cmd + ctrl + 4 / cmd + alt + →
+slate.bind('4:cmd;ctrl', operation.quad[3]);
+slate.bind('right:cmd;alt', operation.quad[3]);
